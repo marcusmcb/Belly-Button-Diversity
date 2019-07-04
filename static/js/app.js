07b934579783500354ec2f6705fe742c6f9ea850
Marcus McBride <marcusmcb@gmail.com>
2019-07-03 18:48:11 -0700

    fixed code to render bubble plot instead of lines & to display selected sample properly

--- static/js/app.js
+++ static/js/app.js
@@ -1,20 +1,20 @@
-// function to build metadata to use in visuals
-function buildMetadata(sample) {
-	let url = `/metadata/$(sample)`;
-	d3.json(url).then(function (sample) {
-		let sampleMetadata = d3.select("#sample-metadata");
-		sampleMetadata.html("");
-		Object.entries(sample).forEach(([key, value]) => {
-			let row = sampleMetadata.append("p");
-			row.text(`${key}: ${value}`)
-		})
+// function to build sample meta data for selector
+function buildMetadata(sample) {	
+	let url = `/metadata/${sample}`;
+	d3.json(url).then(function (sample) {		
+		let sampleMetaData = d3.select("#sample-metadata");				
+		sampleMetaData.html("");
+		Object.entries(sample).forEach(function ([key, value]) {
+			let row = sampleMetaData.append("p");
+			row.text(`${key}: ${value}`);
+		});
 	})
-}
+};
 
-// function to build pie graph
-function buildCharts(sample) {
-	let url = `/samples/${sample}`
-	d3.json(url).then(data => {
+// function to render visuals to html
+function buildCharts(sample) {	
+	let url = `/samples/${sample}`;
+	d3.json(url).then(function (data) {		
 		let xValues = data.otu_ids;
 		let yValues = data.sample_values;
 		let mSize = data.sample_values;
@@ -25,36 +25,37 @@ function buildCharts(sample) {
 			x: xValues,
 			y: yValues,
 			text: tValues,
+			mode: 'markers',
 			marker: {
 				color: mColors,
 				size: mSize
 			}
 		};
 
-		let traceData = [trace1]
+		var data = [trace1];
 
 		let layout = {
-			xaxis: { title: "OTU ID" }
+			xaxis: { title: "OTU ID" },
 		};
 
-		Plotly.newPlot('bubble', traceData, layout);
-
+		Plotly.newPlot('bubble', data, layout);
+		
 		d3.json(url).then(function (data) {
 			let pieValues = data.sample_values.slice(0, 10);
 			let pieLabels = data.otu_ids.slice(0, 10);
 			let pieHover = data.otu_labels.slice(0, 10);
-			let pieData = [{
+			var data = [{
 				values: pieValues,
 				labels: pieLabels,
 				hovertext: pieHover,
 				type: 'pie'
 			}];
-			Plotly.newPlot('pie', pieData)
+			Plotly.newPlot('pie', data);
 		});
 	});
 }
 
-// init function to select data set
+// function for sample selector
 function init() {	
 	let selector = d3.select("#selDataset");	
 	d3.json("/names").then((sampleNames) => {
@@ -71,11 +72,10 @@ function init() {
 	});
 }
 
-// re-renders visuals based on data set selected
 function optionChanged(newSample) {	
 	buildCharts(newSample);
 	buildMetadata(newSample);
 }
 
-// calls init function
+// calls init function above
 init();
